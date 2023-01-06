1325acb6ff4f1ef712cb309f03f8ada00af4859e
mazylol <landonp95@gmail.com>
2023-01-05 21:22:21 -0500

    quotes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center text-gray-200 font-bold mt-16">
         <p class="text-8xl italic">Mazy</p>
-        <p class="text-xl mt-4">"{{ quote() }}" - <i>Landon</i></p>
+        <p class="text-xl mt-4">"{{ quote() }}" - <i>Me</i></p>
     </div>
 </template>
--- utils/quote.ts
+++ utils/quote.ts
@@ -7,7 +7,10 @@ export default function () {
         'On my way to import java.util.*',
         'I can exit Vim. Hire me.',
         '01000011 01101000 01100101 01100101 01110011 01100101 00100001',
-        'I use Arch BTW.'
+        'I use Arch BTW.',
+        'Always hating on Python.',
+        'What is memory management?',
+        'else if else if else if else if'
     ]
 
     return list[Math.floor(Math.random() * list.length)]
